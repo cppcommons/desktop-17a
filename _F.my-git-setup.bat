de681d77e004e97cab73efdd7976f50dc6a8c453
root <root@super-computer>
2017-12-24 14:32:53 +0900

    @

---
+++ _F.my-git-setup.bat
@@ -0,0 +1,7 @@
+set PATH=C:\Users\root\Desktop\cmder-1.3.4\vendor\git-for-windows\cmd;%PATH%
+git config --global user.name  root
+git config --global user.email root@super-computer
+git config --global --unset-all credential.helper
+git config --global credential.helper manager
+git config --global credential.useHttpPath true
+pause
